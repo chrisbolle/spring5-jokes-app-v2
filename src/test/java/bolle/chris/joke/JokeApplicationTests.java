ce1c205a95fe0b6421d8e4fd2fba5d8138275546
Chris Bolle <Chris.Bolle@fnb.co.za>
2022-04-05 08:52:38 +0200

    from initalizr

---
+++ src/test/java/bolle/chris/joke/JokeApplicationTests.java
@@ -0,0 +1,13 @@
+package bolle.chris.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
